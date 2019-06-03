8b4d966454bf9290a537670d353b82be50d29283
nametake <nametake.kyarabuki@gmail.com>
2019-06-03 15:33:12 +0900

    Swithc selected component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,39 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Formik, Form, Field } from 'formik';
 
 import './styles.css';
+
+interface FormValues {
+  selected: string;
+}
+
+const initialValues: FormValues = {
+  selected: 'input'
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Formik<FormValues>
+        initialValues={initialValues}
+        onSubmit={values => {
+          console.log(values);
+        }}
+        render={({ values }) => {
+          return (
+            <Form>
+              <Field name="selected" component="select">
+                <option value="input">Input Form</option>
+                <option value="radio">Radio Button</option>
+              </Field>
+              {(values.selected === 'input' && <div>foo</div>) ||
+                (values.selected === 'radio' && <div>bar</div>)}
+              <button type="submit">submit</button>
+            </Form>
+          );
+        }}
+      />
     </div>
   );
 }
